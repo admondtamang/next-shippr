71b813abbab0e363a980ffedb9815433f0e84380
admondtamang <admondtamang@gmail.com>
2022-02-25 20:56:51 +0545

    [fix] lighbox centered

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
 const colors = require("tailwindcss/colors");
 const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-    content: ["./pages/**/*.{html,js}", "./components/**/*.{html,js}"],
-    theme: {
-        colors: {
-            primary: colors.cyan,
-            secondary: colors.emerald,
-            gray: colors.gray,
-            white: colors.white,
-            black: colors.black,
-            transparent: "transparent",
-        },
-        extend: {
-            fontFamily: {
-                sans: ["Poppins", ...defaultTheme.fontFamily.sans],
-            },
-        },
+  content: ["./pages/**/*.{html,js}", "./components/**/*.{html,js}"],
+  theme: {
+    colors: {
+      primary: colors.cyan,
+      secondary: colors.emerald,
+      gray: colors.gray,
+      white: colors.white,
+      black: colors.black,
+      transparent: "transparent",
     },
-    plugins: [],
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
 };
