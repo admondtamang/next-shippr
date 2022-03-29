541d3e9a8646cc086bdd709df3e04b7f2136ac86
admondtamang <admondtamang@gmail.com>
2022-03-29 10:10:02 +0545

    [fix] style and mobile view

--- components/CustomImage/index.js
+++ components/CustomImage/index.js
@@ -3,14 +3,9 @@ import React from "react";
 import icon from "./icon.png";
 
 export default function CustomImage({ className, ...rest }) {
-  return (
-    <div className="pt-2 imageContainer ">
-      <Image
-        placeholder="blur"
-        blurDataURL={icon}
-        className={`image ${className}`}
-        {...rest}
-      />
-    </div>
-  );
+    return (
+        <div className="pt-2 imageContainer ">
+            <Image placeholder="blur" blurDataURL={icon} className={`image ${className}`} {...rest} />
+        </div>
+    );
 }
--- components/Header/SearchBox.js
+++ components/Header/SearchBox.js
@@ -7,73 +7,67 @@ import { SEARCH_PRODUCTS } from "../../utils/constants";
 import Router from "next/router";
 
 export default function SearchBox() {
-  const [visible, setVisible] = useState(true);
-  const [searchTerm, setSearchTerm] = useState("");
-  const ref = useRef();
+    const [visible, setVisible] = useState(true);
+    const [searchTerm, setSearchTerm] = useState("");
+    const ref = useRef();
 
-  useClickOutside(ref, () => {
-    setVisible(false);
-  });
-
-  //   URL
-  const url = SEARCH_PRODUCTS + searchTerm;
-
-  const {
-    response,
-    error,
-    status: { isLoading, isResolved },
-  } = useFetch(searchTerm ? url : null);
-
-  function handleChange(e) {
-    setVisible(true);
-    setSearchTerm(e.target.value);
-  }
-
-  function onSubmit(e) {
-    e.preventDefault();
-    setVisible(false);
-
-    Router.push({
-      pathname: "/search",
-      query: { searchTerm },
+    useClickOutside(ref, () => {
+        setVisible(false);
     });
-  }
 
-  return (
-    <form
-      onSubmit={onSubmit}
-      className="search_bar relative rounded-full border-2 w-full flex items-center px-2"
-    >
-      <Icon name="search-alt-2" />
+    //   URL
+    const url = SEARCH_PRODUCTS + searchTerm;
 
-      <input
-        className="outline-none border-0 py-2 ml-2 text-sm w-full"
-        type="text"
-        onChange={handleChange}
-        placeholder="Search products"
-      />
+    const {
+        response,
+        error,
+        status: { isLoading, isResolved },
+    } = useFetch(searchTerm ? url : null);
 
-      {/* Submit button */}
-      <Tooltip title="Search" onClick={onSubmit}>
-        <div className="flex-center rounded-full p-1 bg-secondary-500">
-          <Icon name="search-alt-2" color="white" />
-        </div>
-      </Tooltip>
+    function handleChange(e) {
+        setVisible(true);
+        setSearchTerm(e.target.value);
+    }
 
-      {/* View for search data */}
-      <div className="absolute w-full mt-10 rounded-full border-bottom-2 font-bold p-2 bg-white bottom-4 px-4 top-0">
-        <div
-          ref={ref}
-          // border-bottom-2 font-bold hover:text-primary-50 hover:bg-primary-400
-          className={` z-50 ${visible ? "visible" : "invisible"}`}
-        >
-          {isLoading && searchTerm != "" && <p>Loading</p>}
-          {isResolved &&
-            response.map((item) => (
-              <ProductItem item={item} key={item.id} small_product />
-            ))}
-        </div>
-      </div>
-    </form>
-  );
+    function onSubmit(e) {
+        e.preventDefault();
+        setVisible(false);
+
+        Router.push({
+            pathname: "/search",
+            query: { searchTerm },
+        });
+    }
+
+    return (
+        <form onSubmit={onSubmit} className="search_bar relative rounded-full border-2 w-full flex items-center px-2">
+            <Icon name="search-alt-2" />
+
+            <input
+                className="outline-none border-0 py-2 ml-2 text-sm w-full"
+                type="text"
+                onChange={handleChange}
+                placeholder="Search products"
+            />
+
+            {/* Submit button */}
+            <Tooltip title="Search" onClick={onSubmit}>
+                <div className="flex-center rounded-full p-1 bg-secondary-500">
+                    <Icon name="search-alt-2" color="white" />
+                </div>
+            </Tooltip>
+
+            {/* View for search data */}
+            <div className="absolute w-full mt-10 rounded-full border-bottom-2 font-bold p-2 bg-white bottom-4 px-4 top-0">
+                <div
+                    ref={ref}
+                    // border-bottom-2 font-bold hover:text-primary-50 hover:bg-primary-400
+                    className={` z-50 ${visible ? "visible" : "invisible"}`}
+                >
+                    {isLoading && searchTerm != "" && <p>Loading</p>}
+                    {isResolved && response.map((item) => <ProductItem item={item} key={item.id} small_product />)}
+                </div>
+            </div>
+        </form>
+    );
 }
